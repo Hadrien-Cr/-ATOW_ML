dd1275f6273eaaacd2dc046ea07dc6ca7199f802
Hadrien Crassous <hadrien.crassous@student-cs.fr>
2024-10-23 11:59:52 +0200

    add encoding

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pickle
 import os
 import sys
 from preprocessing.country_and_airports_codes import compute_lon_lat,group_and_rename_countries, group_and_rename_airports
-
+from preprocessing.encoding import one_hot_encoding,string_to_value_count, string_to_int_hashing
 def main():
     ########################## DATA LOADING ########################
 
@@ -24,6 +24,21 @@ def main():
     compute_lon_lat(train_df, test_df) # computes lon, lat for each airport
     group_and_rename_countries(train_df, test_df) # simplify country_codes by group and rename countries
     group_and_rename_airports(train_df, test_df) # simplify airport codes by group and rename airport
+
+    ########################## ENCODING #########################
+
+    print("-"*100)
+    print("Start of the encoding ! ")
+    print("-"*100)
+
+    columns_to_ohe = [] # A changer
+    one_hot_encoding(train_df, test_df, columns_to_ohe)
+
+    columns_to_hash = [] # A changer
+    string_to_int_hashing(train_df, test_df, columns_to_hash)
+
+    columns_to_vc = ['country_code_ades', 'country_code_adep', 'adep', 'ades', 'airline'] # A changer
+    string_to_value_count(train_df, test_df, columns_to_vc)
 
     ########################## DATA SPLITTING #######################
 
---
+++ preprocessing/encoding.py
@@ -0,0 +1,67 @@
+import pandas as pd 
+import numpy as np
+from preprocessing.country_and_airports_codes import group_and_rename_countries, group_and_rename_airports, compute_lon_lat
+
+def one_hot_encoding(df_train, df_test, columns):
+    '''
+    One hot encoding for the columns in columns
+    '''
+    for column in columns:
+        df_train[column] = df_train[column].astype('category')
+        df_test[column] = df_test[column].astype('category')
+        df_train = pd.concat([df_train, pd.get_dummies(df_train[column], prefix=column)], axis=1)
+        df_test = pd.concat([df_test, pd.get_dummies(df_test[column], prefix=column)], axis=1)
+        df_train = df_train.drop(column, axis=1)
+        df_test = df_test.drop(column, axis=1)
+
+    print("-"*100)
+    print(f"Columns {columns} sucessfully one hot encoded !")
+    print("-"*100)
+
+
+def string_to_int_hashing(df_train, df_test, columns):
+    '''
+    String to int encoding with arbitrary hashing
+    '''
+    for column in columns:
+        df_train[column] = df_train[column].astype('category')
+        df_test[column] = df_test[column].astype('category')
+        df_test[column] = df_test[column].cat.set_categories(df_train[column].cat.categories)
+
+        df_train[column] = df_train[column].cat.codes
+        df_test[column] = df_test[column].cat.codes
+
+    print(df_test, df_train)
+    print("-"*100)
+    print(f"Columns {columns} successfully string to int encoded!")
+    print("-"*100)
+
+def string_to_value_count(df_train, df_test, columns):
+    '''
+    String to int encoding based on value_counts.
+    The most frequent value gets the smallest integer (0), the second most frequent value gets the second smallest integer (1), etc.
+    '''
+    for column in columns:
+        value_counts = df_train[column].value_counts()
+        value_to_int = {value: i for i, value in enumerate(value_counts.index)}
+
+        df_train[column] = df_train[column].map(value_to_int)
+        df_test[column] = df_test[column].map(value_to_int)
+
+    print(df_test, df_train)
+    print("-" * 100)
+    print(f"Columns {columns} successfully string-to-int encoded using value counts!")
+    print("-" * 100)
+
+
+def test_codes():
+    train_df = pd.read_csv('data/challenge_set.csv')
+    test_df = pd.read_csv('data/submission_set.csv')
+    compute_lon_lat(train_df, test_df)
+    group_and_rename_countries(train_df, test_df)
+    group_and_rename_airports(train_df, test_df)
+    print(train_df.columns)
+    string_to_value_count(train_df, test_df, ['country_code_ades', 'country_code_adep'])
+    
+if __name__ == "__main__":
+    test_codes()
