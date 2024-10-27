908860728e3de96b7fbd318a07967c610fe47658
Leo le Douarec <leo.alex@le.douar.ec>
2024-10-27 14:36:18 +0100

    add GNU licensing and update pipeline

--- main.py
+++ main.py
@@ -2,12 +2,37 @@ import sklearn
 import pandas
 import pickle
 import os
+print(os.getcwd())
 import sys
 from preprocessing.country_and_airports_codes import compute_lon_lat,group_and_rename_countries, group_and_rename_airports, group_and_rename_aircraft_types
 from preprocessing.encoding import one_hot_encoding,string_to_value_count, string_to_int_hashing
 from preprocessing.local_time import add_localtime_to_train_and_test
 
 def main():
+
+    ########################## GNU LICENCE #########################
+
+    text = """
+    XGBoost Model to predict airplane TOW, ATOW ML Contest, PRC Data Challenge.
+    Copyright (C) 2024 Hadrien Crassous, Eymeric Giabicani, Dariia Haryfullina, Léo le Douarec.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+    """
+
+    print(text)
+
+
     ########################## DATA LOADING ########################
 
     print("-"*100)
